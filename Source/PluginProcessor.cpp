53272ffb5b4f2ada03d229bfdd4e4cc3ecf1eca8
Rainwaves <biteintoafirework@gmail.com>
2017-02-10 17:01:59 +0000

    Lab2Code_Commit
    Added basic code from AAP lab 2 to processBlock.  The code is commented
    out, and simply intended to be used as a reference to JUCE syntax for
    writing to the audio buffer.

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -143,6 +143,29 @@ void StereoPannerAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBu
 
         // ..do something to the data...
     }
+
+
+	//________________AAP LAB 2______________________//
+	
+	/*
+	// Retrieve the total number of samples in the buffer for this block
+	int numSamples = buffer.getNumSamples();
+
+	//channelDataL and channelDataR are pointers to arrays of legnth numSamples which
+	//contain the audio for one channel. You repeat this for each channel
+	float *channelDataL = buffer.getWritePointer(0);
+	float *channelDataR = buffer.getWritePointer(1);
+
+	//Loop runs from 0 to number of samples in the block
+	for (int i = 0; i < numSamples; ++i) //!! Why ++i and not i++?
+	{
+		//Reduce the amplitude of each sample in the block for the channels
+		channelDataL[i] = channelDataL[i] * 0.5;
+		channelDataL[i] = channelDataL[i] * 0.5;
+	}
+
+	*/
+
 }
 
 //==============================================================================
